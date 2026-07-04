c2228fac25da2533e8413128774c3d14ed3c94b8
M Mahmoud <mysh264@gmail.com>
2026-07-04 18:14:10 +0000

    Fix PDF controller response types

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Contract;
 use App\Models\Invoice;
 use Barryvdh\DomPDF\Facade\Pdf;
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use Illuminate\Http\Response;
 
 class PdfController extends Controller
 {
-    public function invoice(Invoice $invoice): StreamedResponse
+    public function invoice(Invoice $invoice): Response
     {
         $invoice->loadMissing(['client', 'items.product']);
 
@@ -20,7 +20,7 @@ class PdfController extends Controller
         ])->stream(sprintf('invoice-%s.pdf', $invoice->invoice_number ?? $invoice->id));
     }
 
-    public function contract(Contract $contract): StreamedResponse
+    public function contract(Contract $contract): Response
     {
         $contract->loadMissing(['client', 'items.product']);
 
