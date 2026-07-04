7a5349a46295288d0868668940d4dca89cf6dad4
M Mahmoud <mysh264@gmail.com>
2026-07-04 18:11:30 +0000

    Switch PDF exports to shared document view

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -5,24 +5,28 @@ namespace App\Http\Controllers;
 use App\Models\Contract;
 use App\Models\Invoice;
 use Barryvdh\DomPDF\Facade\Pdf;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class PdfController extends Controller
 {
-    public function invoice(Invoice $invoice): Response
+    public function invoice(Invoice $invoice): StreamedResponse
     {
         $invoice->loadMissing(['client', 'items.product']);
 
-        return Pdf::loadView('pdf.invoice', [
+        return Pdf::loadView('pdf.document', [
+            'title' => sprintf('Invoice %s', $invoice->invoice_number ?? $invoice->id),
+            'type' => 'Invoice',
             'invoice' => $invoice,
         ])->stream(sprintf('invoice-%s.pdf', $invoice->invoice_number ?? $invoice->id));
     }
 
-    public function contract(Contract $contract): Response
+    public function contract(Contract $contract): StreamedResponse
     {
         $contract->loadMissing(['client', 'items.product']);
 
-        return Pdf::loadView('pdf.contract', [
+        return Pdf::loadView('pdf.document', [
+            'title' => sprintf('Contract %s', $contract->id),
+            'type' => 'Contract',
             'contract' => $contract,
         ])->stream(sprintf('contract-%s.pdf', $contract->id));
     }
